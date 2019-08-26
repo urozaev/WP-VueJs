4745781d68f3bef0f5264df70ceb8c54096cc3ed
Max <ip.developer58@gmail.com>
2019-08-26 17:00:45 +0300

    добавление в локалсторэдж

--- src/index.js
+++ src/index.js
@@ -19,163 +19,89 @@ const app = new Vue({
     el: '#app',
     data: function() {
         return {
-            items: users,
+            users,
             item: {name: '',phone: '',birthday: '',role: '',archive: ''},
             edit: false,
             editIndex:-1,
-            name: '',
-            show: false
         }
         },
     mounted: function() {
-        // fetch('http://localhost:3000/users')
-        // .then((res) => {
-        // if(200 <= res.status && res.status < 300) {
-        //     return res;
-        // }
-        // throw new Error(response.statusText);
-        // })
-        // .then(function(response) {
-        // return response.json();
-        // })
-        // .then(function(json) {
-        // json.forEach(user => {
-        //     users.push(user)
-        // });
-        // })
-        // .catch((error) => {console.log(error)})
-
-        // var imp = new Inputmask("+7 (999) 999-9999");
-        // imp.mask(this.$refs.field);
-        // var imd = new Inputmask("##/##/####");
-        // imd.mask(this.$refs.datemask);
+      if (localStorage.getItem('users')) {
+        try {
+          this.users = JSON.parse(localStorage.getItem('users'));
+        } catch(e) {
+          localStorage.removeItem('users');
+        }
+      }
     },
 	methods: {
-		addItem: function(e) {
+		addUser(e) {
       
       if(!this.edit)
         {
-        //   fetch('http://localhost:3000/users', {
-        //     method: 'POST',
-        //     credentials: 'same-origin',
-        //     headers: {
-        //       'Accept': 'application/json',
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(this.item),
-        //   })
-        //   .then((res) => {
-        //     if(200 <= res.status && res.status < 300) {
-        //       return res;
-        //     }
-        //     throw new Error(response.statusText);
-        //   })
-        //   .then((response) => {
-        //     return response.json()
-        //   })
-        //   .then((newUser) => {
-        //     users.push(newUser)
-        //   })
-        //   .then(() => {
-        //     console.log('Пользователь добавлен');
-        //   })
-        //   .catch((error) => {console.log(error)})
+          return;
         } 
-      else 
-        {
-          this.items[this.editIndex] = this.item;
-          this.edit = false;
-          this.editIndex = -1;
-        }
+      this.users[this.editIndex] = this.item;
+      this.edit = false;
+      this.editIndex = -1;
+      this.item = {};
+      this.users.push(this.item);
+      console.log(this.edit)
+      
+      this.saveUsers();
+      
  
       $('#modal').modal('hide');
       e.preventDefault();
-
 		},
-		editItem: function(index) {
+		editItem(index) {
     	this.edit = true;
         this.editIndex = index;
-        this.item = this.items[index];
+        this.item = this.users[index];
+       
 
       $('#modal').modal('show');
     },
-    updateUser: function(){
-    //   let url = `http://localhost:3000/users/${this.item.id}`
-    //   fetch(url, {
-    //       method: 'PATCH',
-    //       credentials: 'same-origin',
-    //       headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json',
-    //         'API-Key': 'secret'
-    //       },
-    //       body: JSON.stringify(this.item),
-    //     })
-    //     .then((res) => {
-    //       if(200 <= res.status && res.status < 300) {
-    //         return res;
-    //       }
-    //       throw new Error(response.statusText);
-    //     })
-    //     .then((response) => {
-    //       return response.json()
-    //     })
-    //     .then(() => {
-    //       console.log('Данные пользователя изменены');
-    //     })
-    //     .catch((error) => {console.log(error)})
+    removeUser(index) {
+      this.users.splice(x, 1);
+      this.saveCats();
     },
-    removeUser: function(){
-    //   let url = `http://localhost:3000/users/${this.item.id}`
-    //   fetch(url, {
-    //     method: 'DELETE'
-    //   })
-    //   .then((res) => {
-    //     if(200 <= res.status && res.status < 300) {
-    //       return res;
-    //     }
-    //     throw new Error(response.statusText);
-    //   })
-    //   .then((response) => {
-    //     return response.json()
-    //   })
-    //   .then(() => {
-    //     console.log('Пользователь удалён');
-    //   })
-    //   .catch((error) => {console.log(error)})
+    saveUsers() {
+      const parsed = JSON.stringify(this.users);
+      localStorage.setItem('users', parsed);
     },
     editCancel: function(index){
       this.item = {name: '',phone: '',birthday: '',role: '',archive: ''};
       this.editIndex = -1;
     },
     filterAll() {
-      this.items = _.filter(users, function (item) {
+      this.users = _.filter(users, function (item) {
           return users;
         })
     },
     filterDesign() {
-      this.items = _.filter(users, function (item) {
+      this.users = _.filter(users, function (item) {
         return item.role === 'designer';
       })
     },
     filterDeveloper() {
-      this.items = _.filter(users, function (item) {
+      this.users = _.filter(users, function (item) {
           return item.role === 'developer';
         })
     },
     filterManager() {
-      this.items = _.filter(users, function (item) {
+      this.users = _.filter(users, function (item) {
           return item.role === 'manager';
         })
     },
     sortByName() {
-      this.items = _.sortBy(this.items, ['name']);
+      this.users = _.sortBy(this.users, ['name']);
     },
     sortByBDate() {
-      this.items = _.sortBy(this.items, ['birthday']);
+      this.users = _.sortBy(this.users, ['birthday']);
     },
     sortByID() {
-        this.items = _.sortBy(this.items, ['id']);
+        this.users = _.sortBy(this.users, ['id']);
     },
   },
   props: {
